f6f32cc1d7bda5fa6d3b3a7552ca6c8bfaf052a0
gthinkspark <glthinkspark@yeah.net>
2019-07-15 17:08:01 +0800

    修改报警详情页视频播放的地址赋值方法

--- Foodsafety_S/src/main/java/com/otec/foodsafety/service/alarm/AlarmServiceImpl.java
+++ Foodsafety_S/src/main/java/com/otec/foodsafety/service/alarm/AlarmServiceImpl.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Maps;
 import com.otec.foodsafety.entity.enterprise.EnterpriseBase;
 import com.otec.foodsafety.mapper.util.PushService;
 import com.otec.foodsafety.service.enterprise.EnterpriseBaseService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,10 +41,40 @@ public class AlarmServiceImpl extends BaseServiceImpl<Alarm, Long> implements Al
 
 	@Override
 	public GridDataModel getGridDataModelByCondition(PageObject po) {
+		String tabType = (String)po.getCondition().get("tabType");
+		if (StringUtils.isNotBlank(tabType)) {
+			if (StringUtils.equalsIgnoreCase("1", tabType)) {
+				po.getCondition().put("eventType", "2");
+				po.getCondition().put("sensorType", "1");
+			} else if (StringUtils.equalsIgnoreCase("2", tabType)) {
+				po.getCondition().put("eventType", "2");
+				po.getCondition().put("sensorType", "2");
+			} else if (StringUtils.equalsIgnoreCase("3", tabType)) {
+				po.getCondition().put("eventType", "1");
+				po.getCondition().put("eventId", "5");
+			} else if (StringUtils.equalsIgnoreCase("4", tabType)) {
+				po.getCondition().put("eventType", "1");
+				po.getCondition().put("eventId", "6");
+			} else if (StringUtils.equalsIgnoreCase("5", tabType)) {
+				po.getCondition().put("eventType", "1");
+				po.getCondition().put("extendSql", " and EVENT_ID in ('1', '2', '3', '4') ");
+			}
+		}
+
 		// 得到所有有效的设备信息
 		Integer totalCount = mapper.countAlarmByCondition(po.getCondition());
 		RowBounds rowBounds = new RowBounds(po.getOffset(), po.getPageSize());
 		List<AlarmExt> results = mapper.findAlarmByCondition(po.getCondition(), rowBounds);
+
+		//20190705解决web视频地址播放问题-修改
+        for(AlarmExt alarmExt:results){
+            if(StringUtils.isNotBlank(alarmExt.getMsg())&&!alarmExt.getMsg().equals("null")){
+                alarmExt.setMsg(alarmExt.getMsg().replaceAll("\"event_video\":\"","\"event_video\":\""+"http://101.132.144.237:8080/event_video/"));
+            }
+            if(StringUtils.isNotBlank(alarmExt.getVideoUrl())&&!alarmExt.getVideoUrl().equals("null")){
+                alarmExt.setVideoUrl("http://101.132.144.237:8080/event_video/"+alarmExt.getVideoUrl());
+            }
+        }
 		return new GridDataModel(results, totalCount.intValue());
 	}
 
