6b09b8eb4afef77883ffb5b8359ca6ebbfd5c704
LG <glthinkspark@yeah.net>
2019-08-18 18:46:21 +0800

    修改BUG

--- Foodsafety_UI/config/dev.env.js
+++ Foodsafety_UI/config/dev.env.js
@@ -1,13 +1,13 @@
 module.exports = {
     NODE_ENV: '"development"',
     CLIENT_UI: '/Foodsafety/',
-    // BASE_API: '"http://localhost:19380/Foodsafety_C"',
+    BASE_API: '"http://localhost:19380/Foodsafety_C"',
     // BASE_API: '"http://106.75.119.205:8089/Foodsafety_C"',
     // BASE_API: '"http://101.132.144.237:8081/Foodsafety_C"',
-    BASE_API: '"http://106.75.119.205:8089/Foodsafety_C"',//奥润服务器
+    // BASE_API: '"http://106.75.119.205:8089/Foodsafety_C"',//奥润服务器
 
     // SERVERIMAGEURL:'"ht1.151:8282"', 
-    SERVERIMAGEURL:'"http://58.49.30.102:8282"',
+    SERVERIMAGEURL:'"http://106.75.119.205:80"',
     CMSIMAGEURL:'"http://106.75.119.205:8089/Foodsafety"',
     // FACEURl:'"http://192.168.1.82:8989/Cloud_service/FaceService"',
     CURRENT_CITY:'"松江区"',
