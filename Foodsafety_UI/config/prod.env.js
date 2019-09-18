ca06988d6223b71c14ea5aed681ec1c0c59068aa
gthinkspark <glthinkspark@yeah.net>
2019-09-18 15:19:11 +0800

    阿里服务正式配置文件

--- Foodsafety_UI/config/prod.env.js
+++ Foodsafety_UI/config/prod.env.js
@@ -12,16 +12,34 @@
 //     PITCH: 60
 // }
 
+// module.exports = {
+//     NODE_ENV: '"production"',
+//     CLIENT_UI: '/Foodsafety/',
+//     // BASE_API: '"http://106.75.119.205:8089/Foodsafety_C"',
+//     BASE_API: '"http://106.75.119.205:8089/Foodsafety_C"', //奥润服务器
+//
+//     // SERVERIMAGEURL:'"http://58.49.30.102:8282"',
+//     SERVERIMAGEURL: '"http://106.75.119.205:80"',
+//     SERVERVIDEOURL: '"http://106.75.119.205:80"',
+//     CMSIMAGEURL: '"http://106.75.119.205:8089/Foodsafety"',
+//     // FACEURl:'"http://192.168.1.82:8989/Cloud_service/FaceService"',
+//     CURRENT_CITY: '"松江区"',
+//     CENTER_POSITION: [121.227640, 31.032480],
+//     MAP_ZOOM: 15,
+//     VIEW_MODE: '"3D"',
+//     PITCH: 60
+// }
+
 module.exports = {
     NODE_ENV: '"production"',
     CLIENT_UI: '/Foodsafety/',
     // BASE_API: '"http://106.75.119.205:8089/Foodsafety_C"',
-    BASE_API: '"http://106.75.119.205:8089/Foodsafety_C"', //奥润服务器
+    BASE_API: '"http://foodsafety.91catv.com:8081/Foodsafety_C"',
 
     // SERVERIMAGEURL:'"http://58.49.30.102:8282"',
-    SERVERIMAGEURL: '"http://106.75.119.205:80"',
-    SERVERVIDEOURL: '"http://106.75.119.205:80"',
-    CMSIMAGEURL: '"http://106.75.119.205:8089/Foodsafety"',
+    SERVERIMAGEURL: '"http://foodsafety.91catv.com:8080"',
+    SERVERVIDEOURL: '"http://foodsafety.91catv.com:8080"',
+    CMSIMAGEURL: '"http://foodsafety.91catv.com:8080"',
     // FACEURl:'"http://192.168.1.82:8989/Cloud_service/FaceService"',
     CURRENT_CITY: '"松江区"',
     CENTER_POSITION: [121.227640, 31.032480],
